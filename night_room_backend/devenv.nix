bfdbe3e046471790014bb47180cde29c23f9e6af
stardust-zip <nguyenhieu@proton.me>
2025-07-19 22:14:47 +0700

    Change Django backend to Golang

--- night_room_backend/devenv.nix
+++ night_room_backend/devenv.nix
@@ -1,53 +1,27 @@
-{ pkgs, lib, config, ...}:
+{pkgs, ...}:
+
 {
-  languages.python = {
+  languages.go = {
     enable = true;
-    package = pkgs.python313;
-    uv = {
-      enable = true;
-      sync.enable = true;
-    };
+    package = pkgs.go;
   };
 
   packages = [
-    pkgs.postgresql_16
-    pkgs.python313Packages.python-lsp-server
-    pkgs.python313Packages.pyflakes
-    pkgs.python313Packages.black
-
-    # requirements.txt
-    pkgs.python313Packages.django
-    pkgs.python313Packages.djangorestframework-stubs
-    pkgs.python313Packages.djangorestframework-simplejwt
+    pkgs.golangci-lint
+    pkgs.gopls
+    pkgs.go-tools
+    pkgs.delve
   ];
 
-
-
-  services.postgres = {
-    enable = true;
-    package = pkgs.postgresql_16;
+  processes.backend = {
+    exec = "go run ./cmd/server";
   };
 
   enterShell = ''
-    echo "--- 🐍 Welcome to the Backend Shell 🐍 ---"
-    python --version
-    pip --version
-
-    echo ""
-    echo "✅ Dependencies from requirements.txt have been installed."
-
-    echo ""
-    echo "🐘 PostgreSQL Info:"
-    echo "   Service is running in the background."
-    echo "   To connect manually, run: psql ''${config.services.postgres.psqlConnectionString}"
-
-    echo ""
-    echo "🚀 Next Steps:"
-    echo "   1. If this is the first time, start your Django project: django-admin startproject myproject ."
-    echo "   2. Apply database migrations: python manage.py migrate"
-    echo "   3. Run the development server: python manage.py runserver"
-    echo "-----------------------------------------------"
+    echo "Welcome to Night Room backend environment"
+    echo "Go version: $(go version)"
+    echo "To run the backend server in the background, use devenv up"
+    export PS1="Golang DEVENV $PS1"
   '';
-
 
 }
